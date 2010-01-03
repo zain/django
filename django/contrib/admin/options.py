9dadfe623076bd9625828944a06603112b0bc8b2
Zain Memon <zain@inzain.net>
2010-01-02 16:42:55 -0800

    OrderField, inline reordering in the admin, docs, and tests. (commit #2)

--- django/contrib/admin/options.py
+++ django/contrib/admin/options.py
@@ -1110,6 +1110,7 @@ class InlineModelAdmin(BaseModelAdmin):
     formset = BaseInlineFormSet
     extra = 3
     max_num = 0
+    order_field = None
     template = None
     verbose_name = None
     verbose_name_plural = None
@@ -1131,6 +1132,9 @@ class InlineModelAdmin(BaseModelAdmin):
             js.append('js/urlify.js')
         if self.filter_vertical or self.filter_horizontal:
             js.extend(['js/SelectBox.js' , 'js/SelectFilter2.js'])
+        if self.order_field:
+            js.extend(['js/jquery-tablednd.js', 'js/jquery-disable.text.select.pack.js', 
+                'js/jquery-listreorder.js'])
         return forms.Media(js=['%s%s' % (settings.ADMIN_MEDIA_PREFIX, url) for url in js])
     media = property(_media)
 
